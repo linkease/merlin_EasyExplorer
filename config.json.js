462eaf08bfc3d6beb095475a0b1da8be08ec8739
clangcn <clangcn@gmail.com>
2018-09-21 19:53:03 +0800

    提升网络传输效率

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "EasyExplorer 跨设备文件同步+DLNA流媒体", 
     "home_url": "Module_easyexplorer.asp", 
-    "md5": "52883b415bb876c8caf5e64e78402d9c", 
+    "md5": "97fa898074b5279b92e32d906d63a70b", 
     "module": "easyexplorer", 
     "title": "EasyExplorer文件同步", 
-    "version": "0.0.7"
+    "version": "0.0.8"
 }
