96c7a3a9c344ee3e05ea6bec8ab5275676952bac
jannson <gandancing@gmail.com>
2018-12-09 11:56:51 +0800

    upgrade to binaries 20181209.033820

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "EasyExplorer 跨设备文件同步+DLNA流媒体", 
     "home_url": "Module_easyexplorer.asp", 
-    "md5": "97fa898074b5279b92e32d906d63a70b", 
+    "md5": "645fbf6a0da85740f8f5fac310726109", 
     "module": "easyexplorer", 
     "title": "EasyExplorer文件同步", 
-    "version": "0.0.8"
+    "version": "0.0.9"
 }
