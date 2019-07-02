18e827d3ce78904ee4109cd634579b8b81f0fb5e
sadoneli <sadoneli@gmail.com>
2019-07-02 15:09:18 +0800

    update EE

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
-    "description": "易有云 （EasyExplorer） 跨平台文件同步", 
+    "description": "易有云 （EasyExplorer） 跨平台文件同步，支持双向同步！", 
     "home_url": "Module_easyexplorer.asp", 
-    "md5": "3229a258a61b6e7f8ec82cd01b86c64e", 
+    "md5": "961536709e67eeea8b6e34c1fe2591d2", 
     "module": "easyexplorer", 
     "title": "易有云", 
-    "version": "1.3"
+    "version": "1.4"
 }
