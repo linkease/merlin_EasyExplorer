350a647507c88c023eb63ceb796f5bf208177aae
sadoneli <sadoneli@gmail.com>
2019-07-03 22:05:12 +0800

    update ee logo

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "易有云 （EasyExplorer） 跨平台文件同步，支持双向同步！", 
     "home_url": "Module_easyexplorer.asp", 
-    "md5": "186f59172c510f245af1052c60f3f161", 
+    "md5": "9ffac66ab4f182b9c96135955f133e82", 
     "module": "easyexplorer", 
     "title": "易有云", 
     "version": "1.5"
