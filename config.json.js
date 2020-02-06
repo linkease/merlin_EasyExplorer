ecb3028277a9e697d70692d1420ccfbc86bb9304
clangcn <clangcn@gmail.com>
2020-02-06 20:24:07 +0800

    更新主程序20200206.024836

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云 （EasyExplorer） 跨平台文件同步，支持双向同步！", 
     "home_url": "Module_easyexplorer.asp", 
-    "md5": "ab4d477f7363751c5341302036b84da8", 
+    "md5": "92cc26ac0e5c6f68a283258384ca0f56", 
     "module": "easyexplorer", 
     "title": "易有云", 
-    "version": "1.6"
+    "version": "1.6.1"
 }
